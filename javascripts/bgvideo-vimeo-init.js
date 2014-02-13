14fb34e2396967f5f2d3cd48f8137a231e88c7ba
ozma77 <ozma77@gmail.com>
2014-02-12 20:10:53 -0500

    Template added
    Added files for the site to check if it works!

---
+++ javascripts/bgvideo-vimeo-init.js
@@ -0,0 +1,40 @@
+// BGVIDEO-INIT.JS
+//--------------------------------------------------------------------------------------------------------------------------------
+//This is  JS file that activates element animation effects used in this template*/
+// -------------------------------------------------------------------------------------------------------------------------------
+// Template Name: DIGNITY.
+// Author: Designova.
+// Website: http://www.designova.net 
+// Copyright: (C) 2013 
+// -------------------------------------------------------------------------------------------------------------------------------
+
+/*global $:false */
+/*global window: false */
+
+(function(){
+  "use strict";
+
+
+$(function ($) {
+
+   		
+        $.okvideo({ source: '31241154', //set your vimeo video source here
+                    loop: true,
+                    hd:true, 
+                    adproof: true
+                 });
+   
+});
+// $(function ($)  : ends
+
+})();
+//  JSHint wrapper $(function ($)  : ends
+
+
+
+
+
+
+
+	
+
